cd318d41bd05bc29b3d405a5bfa267dd5e060690
Cem Gökmen <cem@cemgokmen.com>
2017-10-21 01:16:27 -0400

    Fields forced to not null

--- ratsightings/models.py
+++ ratsightings/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class RatSighting(models.Model):
-    owner = models.ForeignKey(User, related_name='ratsightings', on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, related_name='ratsightings', on_delete=models.CASCADE, null=False)
     date_created = models.DateTimeField(auto_now_add=True, null=False)
-    location_type = models.CharField(max_length=100)
-    zip_code = models.IntegerField()
-    address = models.TextField(max_length=500)
-    city = models.CharField(max_length=100)
-    borough = models.CharField(max_length=100)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    location_type = models.CharField(max_length=100, null=False)
+    zip_code = models.IntegerField(null=False)
+    address = models.TextField(max_length=500, null=False)
+    city = models.CharField(max_length=100, null=False)
+    borough = models.CharField(max_length=100, null=False)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, null=False)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=False)
 
     def __str__(self):
         return "%s, %s, %s %d" % (self.address, self.borough, self.city, self.zip_code)
