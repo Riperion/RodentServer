de4da2240bc2f042c91c1dcb6493f6a2b654187d
Cem Gökmen <cem@cemgokmen.com>
2017-10-30 22:18:39 -0400

    Added default backend

--- rodent/settings.py
+++ rodent/settings.py
@@ -52,6 +52,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
     'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
