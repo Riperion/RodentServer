fadbb2f78b4affee4adbe8d1eb9e466bc68d2f64
Cem Gökmen <cem@cemgokmen.com>
2017-10-30 22:09:27 -0400

    Added date filter

--- ratsightings/views.py
+++ ratsightings/views.py
@@ -1,8 +1,16 @@
 # Create your views here.
+from django_filters.rest_framework import FilterSet, DateFromToRangeFilter
+
 from .models import RatSighting
 from .serializers import RatSightingSerializer
 from rest_framework import viewsets
 
+class RatSightingDateRangeFilter(FilterSet):
+    date = DateFromToRangeFilter()
+
+    class Meta:
+        model = RatSighting
+        fields = ['date_created']
 
 class RatSightingViewSet(viewsets.ModelViewSet):
     """
@@ -10,6 +18,7 @@ class RatSightingViewSet(viewsets.ModelViewSet):
     """
     queryset = RatSighting.objects.all()
     serializer_class = RatSightingSerializer
+    filter_class = RatSightingDateRangeFilter
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- rodent/settings.py
+++ rodent/settings.py
@@ -51,6 +51,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
+    'DEFAULT_FILTER_BACKENDS': (
+        'rest_framework_filters.backends.DjangoFilterBackend'
+    ),
     'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
