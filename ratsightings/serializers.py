7fc861715c1db254a82e09a0acfa9add714c1a89
Cem Gökmen <cem@cemgokmen.com>
2017-11-05 15:43:35 -0500

    Added stats view

--- ratsightings/serializers.py
+++ ratsightings/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import fields
 from rest_framework import serializers
 from .models import RatSighting
 
@@ -7,3 +8,10 @@ class RatSightingSerializer(serializers.ModelSerializer):
     class Meta:
         model = RatSighting
         fields = ('id', 'owner', 'date_created', 'location_type', 'zip_code', 'address', 'city', 'borough', 'latitude', 'longitude')
+
+class RatSightingStatsQuerySerializer(serializers.Serializer):
+    from_year = fields.IntegerField(min_value=1900, max_value=2100)
+    from_month = fields.IntegerField(min_value=1, max_value=12)
+
+    to_year = fields.IntegerField(min_value=1900, max_value=2100)
+    to_month = fields.IntegerField(min_value=1, max_value=12)
--- ratsightings/views.py
+++ ratsightings/views.py
@@ -1,8 +1,14 @@
 # Create your views here.
 from django_filters.rest_framework import FilterSet, DateFromToRangeFilter
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import datetime
+from dateutil.relativedelta import relativedelta
 
 from .models import RatSighting
-from .serializers import RatSightingSerializer
+from .serializers import RatSightingSerializer, RatSightingStatsQuerySerializer
 from rest_framework import viewsets
 
 class RatSightingDateRangeFilter(FilterSet):
@@ -22,3 +28,42 @@ class RatSightingViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+class RatSightingStatsView(APIView):
+    def get(self, request, format=None):
+        # Get the dates sent by the user
+        serializer = RatSightingStatsQuerySerializer(data=request.QUERY_PARAMS)
+        if not serializer.is_valid():
+            return Response(
+                data=serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        data = serializer.object
+
+        from_date = datetime.date(year=data.from_year, month=data.from_month, day=1)
+        to_date = datetime.date(year=data.to_year, month=data.to_month, day=1)
+
+        if from_date >= to_date:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # Get date frame tuples for each month involved in date range
+        ranges = []
+
+        current = from_date
+        while current < to_date:
+            next_date = from_date + relativedelta(months=1)
+            ranges.append((current, next_date))
+
+            current = next_date
+
+
+        # Map the results into a dictionary
+        results = {}
+
+        for range_from, range_to in ranges:
+            query = RatSighting.objects.filter(date_created__gte=range_from, date_created__lt=range_to)
+            results[range_from.strftime("%Y-%m")] = query.count()
+
+        return Response(results)
